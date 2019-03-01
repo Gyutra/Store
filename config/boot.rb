a25b4369eae14e3d84c8984311cd27b6ed57a306
Дмитро <44162560+Gyutra@users.noreply.github.com>
2019-03-01 15:23:47 +0200

    Update boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,13 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+  def update
+    @category = Category.find(params[:id])
+
+      if @category.update(category_params)
+        redirect_to @category
+      else
+        render 'edit' 
+      end			
+  end
